6aa3f4a11836be8023c311355e7c4f395a8940de
Meer Quais <meerquais017@gmail.com>
2024-08-08 23:10:45 +0500

    javascript code

---
+++ 03-08-2024/app.js
@@ -0,0 +1,59 @@
+// alert("this is alert");
+
+// window.alert("this is window alert")
+
+// var myname;
+// var countryname;
+// // console.log(myname);
+
+// myname = "Khan";
+// countryname = "Pakistan";
+// alert(myname);
+
+// var _weightkilo = "150";
+// var weight_kilo = "150";
+// var weightKilo = "150";
+// var weightKilo150 = "150";
+// var $weightKilo150 = "150";
+// var weightKilo150$ = "150";
+
+// var weight = 100;
+// var Weight = 150;
+
+// var weight1 = 100;
+// var total = weight + weight1;
+// alert(total);
+// console.log(total);
+
+// var whatsLeftOver = 2 % 4;
+
+// var num1 = 5;
+// var num2 = 5;
+// var newNumPost = num1++;
+// var newNumPre = ++num2;
+
+// console.log(newNumPost, newNumPre);
+
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost);
+// var userName = "ashir";
+// var message = "Thanks" + " " + userName + " " + "for your input!";
+// console.log(message);
+
+// var teaher = "zuhaib";
+// var teaher1 = "ashir";
+
+// console.log(teaher - teaher1)
+
+// var userName = prompt("Your name?", "Zuhaib");
+// var message = "Wellcome" + " " + userName;
+// console.log(message);
+var num = 3;
+var cond = (num == 2);
+console.log("Condition", cond);
+
+// if (condi) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
