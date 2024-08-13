b7de431a19ccd8fcb18f2480223008d36bc0a864
zuhaibahmed457 <zuhaibahmed0007@gmail.com>
2024-08-13 22:43:31 +0500

    third class

---
+++ class-3/app.js
@@ -0,0 +1,38 @@
+// var number = 100;
+// number = "String";
+
+// console.log(number);
+
+// if (undefined == "") {
+//   console.log("pikachu");
+// } else {
+//   console.log("else");
+// }
+
+var fullName = "Mark Myers";
+
+// if (fullName === "Mark" + " " + "Myers") {
+
+// }
+if (fullName === "firstName + " + " + lastName") {
+}
+// if ("String" !== 100) {
+
+// }
+
+if (200 >= 200) {
+}
+if (200 <= 200) {
+}
+if ("200" === 200) {
+}
+var num = prompt("Enter Number");
+if (num == 200) {
+  console.log("IF", num);
+} else if (num < 200) {
+  console.log("ElSE IF", num);
+} else if (num < 150) {
+  console.log("ElSE IF 150", num);
+} else {
+  console.log("Else", num);
+}
