4ec0a1c08b82f5d0a9eb242a7f24fe9bff9f0dbe
zuhaibahmed457 <zuhaibahmed0007@gmail.com>
2024-08-15 22:48:54 +0500

    class 4

---
+++ class-4/app.js
@@ -0,0 +1,68 @@
+// git status
+// git add --all or filaname
+// git commit -m "your message"
+// git push
+// ---------------
+
+// git config --global user.name "username"
+// git config --global user.email "useremail"
+
+// ----------------------------------------------------
+
+// "=="
+
+// var result = 12 == "12";
+// console.log(result); true
+
+// "==="
+
+// var result = 12 === "12";
+// console.log(result); false
+
+// && End
+// || OR
+
+// var userPrompt = 1000;
+// if (userPrompt < 0 || userPrompt < 100) {
+//     console.log("marks are not less than 0")
+// }  else {
+
+// }
+
+//  &&
+if (false && true) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+//  ||
+
+// if (false || false || false) {
+//   console.log("if");
+// } else {
+//   console.log("else");
+// }
+
+// Nested
+
+// if (true) {
+//      if(){
+//      }
+
+// } else {
+
+// }
+
+// Arrays
+
+// var city1 = "Karachi"
+// var city2 = "Lahore"
+// var city3 = "Islambad"
+// var city4 = "Faislabad"
+// var city5 = "Chichawatni"
+// var city6 = "Quetta"
+
+var cityNames = ["Karachi", 0, "Islambad", "Faislabad", "Quetta"];
+
+console.log(cityNames[4], cityNames[1]);
