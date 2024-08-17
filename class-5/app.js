53a239b099472df383feebdb6fdd97cb8e6e2d7f
zuhaibahmed457 <zuhaibahmed0007@gmail.com>
2024-08-17 22:57:16 +0500

    class no 5

---
+++ class-5/app.js
@@ -0,0 +1,38 @@
+// var array = ["a", "b", 6, "d", "e"];
+// console.log("1", array.length);
+
+// array = [];
+// console.log("2", array);
+
+// var array = [];
+
+// array[0] = "a";
+// array[5] = "b";
+
+// console.log(array.length);
+
+// var array = [];
+
+// // var removeValue = array.pop();
+// // ["a", "b", 6, "d"];
+// // var removeValue2 = array.pop();
+
+// console.log(array, removeValue);
+
+// var array = ["a", "b", 6, "d", "e"];
+
+// var result = array.push("first", "second", "third", 8);
+
+// console.log(array[8], result);
+
+// var kuchbhi = ["a", "b", 6, "d", "e"];
+
+// var result = kuchbhi.unshift("ccc", "zzz");
+
+// console.log(kuchbhi, result);
+
+var kuchbhi = ["a", "b", 6, "d", "e"];
+
+var result = kuchbhi.splice(2, 2, "G", "H");
+
+console.log(kuchbhi, result);
