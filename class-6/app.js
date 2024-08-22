8987bc6483bd935b26dfe81d77d17c75cf508a98
zuhaibahmed457 <zuhaibahmed0007@gmail.com>
2024-08-22 22:50:04 +0500

    clas 7

---
+++ class-6/app.js
@@ -0,0 +1,9 @@
+// var kuchbhi = ["a", "b", 6, "d", "e"];
+// var result = kuchbhi.slice(0, 2);
+// console.log(result, kuchbhi);
+
+var kuchbhi = ["a", "b", 6, "d", "e"];
+
+for (var i = 1; i <= 6; i++) {
+      console.log(i*2);
+}
---
+++ class-7/app.js
@@ -0,0 +1,37 @@
+// var arr = [1, 22, 30, 4, 100, 6];
+// for (var i = 0; i < 10; ++i) {
+//   console.log(i);
+// }
+
+// // arr = [2, 3, 4, 5, 6, 6]
+// console.log(arr);
+
+// arr.length = arr.length - 1;
+
+// console.log(arr);
+
+// Shift
+
+// 1: remove first value from array
+// 2: return deleted value
+
+var arr = [1, 2, 3, 4, 5, 6];
+var match = "no";
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] == 4) {
+//     match = "yes";
+//     break;
+//   }
+
+//   console.log(i);
+// }
+
+// console.log(match);
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+  for (var j = 0; j < 10; j++) {
+    console.log(j);
+  }
+}
