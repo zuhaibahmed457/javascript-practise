3ba05ca4ebeffe3d5f997a99abfa998f0dadb3c7
zuhaibahmed457 <zuhaibahmed0007@gmail.com>
2024-08-24 22:58:22 +0500

    class 8

---
+++ class-8/app.js
@@ -0,0 +1,28 @@
+// var arr = [1, 2, ["a", "b", "c"], 3];
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i].length) {
+//     for (var j = 0; j < arr[i].length; j++) {
+//       console.log(arr[i][j]);
+//     }
+//     break;
+//   }
+//   console.log(arr[i]);
+// }
+
+// var cityName = prompt("Your City");
+// var cities = ["Karachi", "Lahore", "Islamabad"];
+// for (var i = 0; i < cities.length; i++) {
+//   if (cityName.toLowerCase() == cities[i]) {
+//     console.log("City found", cityName);
+//     break;
+//   } else {
+//     console.log("Change your city", cityName);
+//   }
+// }
+
+// var city = "Karachi";
+// var result = city.slice(0, 2);
+
+// console.log(result);
+
+var arr = ["a", "e", "d", "t"];
